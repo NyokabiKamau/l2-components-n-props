d1754445d2a4121badacca8d5645fcf4b2104ce1
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-01-10 12:20:02 +0300

    edited

--- src/components/card-class/CardA.js
+++ src/components/card-class/CardA.js
@@ -1,21 +1,10 @@
 import React from "react";
 
+class CardA extends React.Component {
 
-class CardA extends React.Component{
-
-    render(){
-
-        return (
-            <div className="card">
-                <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-female-battle-royale-flaticons-lineal-color-flat-icons.png" alt="avatar" className="image"/>
-                <div class="container">
-                    <h4><b>Jane Doe</b></h4>
-                    <p>Software Engineer</p>
-                </div>
-            </div>
-        )
+    render() {
+        return <p>Hello from class component</p>
     }
 }
-
 
 export default CardA
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/app/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/App"
+import CardA from "./components/card-class/CardA"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = document.getElementById("app")
+const main = ReactDOM.createRoot(app)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// display page
+main.render(
+  <div>
+    <h1>Hello XYZ</h1>
+    <App/>
+    <CardA/>
+  </div>
+  
+
+)
